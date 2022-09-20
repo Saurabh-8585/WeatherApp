fb87f04d99832739d5166e219fe3fcff12562659
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2022-09-20 13:07:13 +0530

    new features

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,6 +1,15 @@
-import axios from 'axios'
 import '../css/card.css'
-import { useState } from 'react'
+import sunset from '../assets/sunset.gif'
+import sunrise from '../assets/sunrise.gif'
+import hot from '../assets/hot.gif'
+import cold from '../assets/cold.gif'
+import cloudy from '../assets/cloudy.gif'
+import humidity from '../assets/humidity.png'
+import pressure from '../assets/pressure.png'
+import wind from '../assets/wind.gif'
+import axios from 'axios'
+import moment from 'moment'
+import React, { useState } from 'react'
 
 function Card() {
     const [city, setCity] = useState('');
@@ -16,14 +25,16 @@ function Card() {
     })
     const handleOnClick = () => {
         axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d8596b1261b43be39522177d29112a96`).then((response) => {
+            console.log(response.data);
             setData({
-                desc:response.data.weather[0].main,
-                icon:response.data.weather[0].icon,
+                desc: response.data.weather[0].main,
+                icon: response.data.weather[0].icon,
                 city: response.data.name,
                 temp: response.data.main.temp,
                 temp_max: response.data.main.temp_max,
                 temp_min: response.data.main.temp_min,
                 humidity: response.data.main.humidity,
+                pressure: response.data.main.pressure,
                 sunrise: response.data.sys.sunrise,
                 sunset: response.data.sys.sunset,
                 country: response.data.sys.country,
@@ -59,7 +70,7 @@ function Card() {
                                 setCity(event.target.value)
                             }}
                         />
-                        
+
                         <button className='btn' onClick={handleOnClick} type='submit'>Search</button>
                     </div>
                     {
@@ -73,38 +84,76 @@ function Card() {
                                     <h2>0 °C</h2>
                                 </div>
                                 <div className="boxes">
-                                    <div className="box"><h4>Max Temp 
-                                        <span>  <i className="fa-solid fa-temperature-arrow-up"></i></span></h4>
+                                    <div className="box"><h4>Max Temp</h4>
                                         <h3>{0} °C</h3></div>
-                                    <div className="box"><h4>Min Temp
-                                        <span>  <i className="fa-solid fa-temperature-arrow-down"></i></span></h4>
+                                    <div className="box"><h4>Min Temp</h4>
                                         <h3>{0} °C</h3></div>
                                     <div className="box">
-                                        <h4>Humidity</h4>
-                                        <h2>{0}</h2></div>
+                                        <h3>Humidity</h3>
+                                        <h3>{0}</h3></div>
                                     <div className="box"><h3>Feels like</h3>
                                         <h2>{"--"}</h2></div>
+                                    <div className="box"><h3>Sunrise</h3>
+                                        <h3>{"--"}</h3></div>
+                                    <div className="box"><h3>Sunset
+                                        <i className="fa-solid fa-sun-haze"></i>
+                                    </h3>
+                                        <h3>{"--"}</h3></div>
+                                    <div className="box"><h3>Pressure</h3>
+                                        <h3>{"--"}</h3></div>
+                                    <div className="box"><h3>Wind Speed</h3>
+                                        <h3>{"--"}</h3></div>
                                 </div>
                             </>
                         ) : (
                             <>
-                                    <div className="city">
+                                <div className="city">
                                     <h3><i className="fa-sharp fa-solid fa-location-dot"></i> {data.city}  {data.country}</h3>
                                 </div>
                                 <div className="info">
                                     <h2>{data.temp} °C</h2>
                                 </div>
                                 <div className="boxes">
-                                        <div className="box"><h4>Max Temp
-                                            <span>  <i className="fa-solid fa-temperature-arrow-up"></i></span></h4>
+                                    <div className="box"><h4>Max Temp</h4>
+                                        <div className="border">
+                                            <img src={hot} className="img" alt="max_temp" />
+                                        </div>
                                         <h3>{data.temp_max} °C</h3></div>
-                                        <div className="box"><h4>Min Temp
-                                            <span>  <i className="fa-solid fa-temperature-arrow-down"></i></span></h4>
+                                    <div className="box"><h4>Min Temp</h4>
+                                        <div className="border">
+                                            <img src={cold} className="img" alt="min_temp" />
+                                        </div>
                                         <h3>{data.temp_min} °C</h3></div>
-                                        <div className="box"><h4>Humidity</h4>
+                                    <div className="box"><h3>Humidity</h3>
+                                        <div className="border">
+                                            <img src={humidity} className="img" alt="humidity" />
+                                        </div>
                                         <h3>{data.humidity}</h3></div>
                                     <div className="box"><h3>Feels Like</h3>
-                                            <h3>{data.desc}</h3></div>
+                                        <div className="border">
+                                            <img src={cloudy} className="img" alt="weather" />
+                                        </div>
+                                        <h3>{data.desc}</h3></div>
+                                    <div className="box rise"><h3>Sunrise</h3>
+                                        <div className="border">
+                                            <img src={sunrise} className="img" alt="sunrise" />
+                                        </div>
+                                        <h3>{moment(data.sunrise * 1000).format("hh:mm a")}</h3></div>
+                                    <div className="box"><h3>Sunset</h3>
+                                        <div className="border">
+                                            <img src={sunset} className="img" alt="sunset" />
+                                        </div>
+                                        <h3>{moment(data.sunset * 1000).format("hh:mm a")}</h3></div>
+                                    <div className="box"><h3>Pressure</h3>
+                                        <div className="border">
+                                            <img src={pressure} className="img" alt="pressure" />
+                                        </div>
+                                        <h3>{data.pressure}</h3></div>
+                                    <div className="box"><h3>Wind Speed</h3>
+                                        <div className="border">
+                                            <img src={wind} className="img" alt="sunrise" />
+                                        </div>
+                                        <h3>{data.windSpeed}</h3></div>
                                 </div>
                             </>
                         )}
