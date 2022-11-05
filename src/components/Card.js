fb86ea3e7dd95f8b22edf431acfd06c6d1b5ba60
Saurabh Khatmode <khatmodesaurabh7@gmail.com>
2022-11-05 13:59:55 +0530

    minor

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,3 +1,4 @@
+
 import '../css/card.css'
 import sunset from '../assets/sunset.gif'
 import sunrise from '../assets/sunrise.gif'
@@ -12,7 +13,8 @@ import moment from 'moment'
 import React, { useState } from 'react'
 
 function Card() {
-    const [city, setCity] = useState('');
+    const [defaultCity, setDefaultCity] = useState('Pune');
+    const [timeGap, setTimeGap] = useState("")
     const [data, setData] = useState({
         city: "",
         temp: 0,
@@ -23,24 +25,35 @@ function Card() {
         sunset: 0,
         country: ""
     })
-    const handleOnClick = () => {
-        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d8596b1261b43be39522177d29112a96`).then((response) => {
-            setData({
-                desc: response.data.weather[0].main,
-                icon: response.data.weather[0].icon,
-                city: response.data.name,
-                temp: response.data.main.temp,
-                temp_max: response.data.main.temp_max,
-                temp_min: response.data.main.temp_min,
-                humidity: response.data.main.humidity,
-                pressure: response.data.main.pressure,
-                sunrise: response.data.sys.sunrise,
-                sunset: response.data.sys.sunset,
-                country: response.data.sys.country,
-                windSpeed: response.data.wind.speed
+    const getData = (city) => {
+        axios.get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=d8596b1261b43be39522177d29112a96`)
+            .then((response) => {
+                setData({
+                    desc: response.data.weather[0].main,
+                    icon: response.data.weather[0].icon,
+                    city: response.data.name,
+                    temp: response.data.main.temp,
+                    temp_max: response.data.main.temp_max,
+                    temp_min: response.data.main.temp_min,
+                    humidity: response.data.main.humidity,
+                    pressure: response.data.main.pressure,
+                    sunrise: response.data.sys.sunrise,
+                    sunset: response.data.sys.sunset,
+                    country: response.data.sys.country,
+                    windSpeed: response.data.wind.speed
+                })
             })
-        })
     }
+
+    const handleOnChange = (e) => {
+        clearTimeout(timeGap)
+        const timeout = setTimeout(() => {
+            getData(e.target.value)
+
+        }, 500)
+        setTimeGap(timeout)
+    }
+    // getData()
     return (
         <div>
             <div className="area" >
@@ -64,100 +77,65 @@ function Card() {
                             type="text"
                             className='inputField'
                             placeholder='Enter City Name'
-                            value={city}
-                            onChange={(event) => {
-                                setCity(event.target.value)
-                            }}
+                            onChange={handleOnChange}
                         />
 
-                        <button className='btn' onClick={handleOnClick} type='submit'>Search</button>
+                        {/* <button className='btn' onClick={handleOnClick} type='submit'>Search</button> */}
                     </div>
-                    {
-                        !city ? (
-                            <>
 
-                                <div className="city">
-                                    <h3> Please Enter the name of city</h3>
+                    <>
+                        <div className="city">
+                            <h3><i className="fa-sharp fa-solid fa-location-dot"></i> {data.city}  {data.country}</h3>
+                        </div>
+                        <div className="info">
+                            <h2>{data.temp} °C</h2>
+                        </div>
+                        <div className="boxes">
+                            <div className="box"><h4>Max Temp</h4>
+                                <div className="border">
+                                    <img src={hot} className="img" alt="max_temp" />
                                 </div>
-                                <div className="info">
-                                    <h2>0 °C</h2>
+                                <h3>{data.temp_max} °C</h3></div>
+                            <div className="box"><h4>Min Temp</h4>
+                                <div className="border">
+                                    <img src={cold} className="img" alt="min_temp" />
                                 </div>
-                                <div className="boxes">
-                                    <div className="box"><h4>Max Temp</h4>
-                                        <h3>{0} °C</h3></div>
-                                    <div className="box"><h4>Min Temp</h4>
-                                        <h3>{0} °C</h3></div>
-                                    <div className="box"><h3>Wind Speed</h3>
-                                        <h3>{"--"}</h3></div>
-                                  
-                                    <div className="box"><h3>Feels like</h3>
-                                        <h2>{"--"}</h2></div>
-                                    <div className="box"><h3>Sunrise</h3>
-                                        <h3>{"--"}</h3></div>
-                                    <div className="box"><h3>Sunset
-                                        <i className="fa-solid fa-sun-haze"></i>
-                                    </h3>
-                                        <h3>{"--"}</h3></div>
-                                    <div className="box"><h3>Pressure</h3>
-                                        <h3>{"--"}</h3></div>
-                                    <div className="box">
-                                        <h3>Humidity</h3>
-                                        <h3>{0}</h3></div>
+                                <h3>{data.temp_min} °C</h3></div>
+                            <div className="box"><h3>Wind Speed</h3>
+                                <div className="border">
+                                    <img src={wind} className="img" alt="sunrise" />
                                 </div>
-                            </>
-                        ) : (
-                            <>
-                                <div className="city">
-                                    <h3><i className="fa-sharp fa-solid fa-location-dot"></i> {data.city}  {data.country}</h3>
-                                </div>
-                                <div className="info">
-                                    <h2>{data.temp} °C</h2>
-                                </div>
-                                <div className="boxes">
-                                    <div className="box"><h4>Max Temp</h4>
-                                        <div className="border">
-                                            <img src={hot} className="img" alt="max_temp" />
-                                        </div>
-                                        <h3>{data.temp_max} °C</h3></div>
-                                    <div className="box"><h4>Min Temp</h4>
-                                        <div className="border">
-                                            <img src={cold} className="img" alt="min_temp" />
-                                        </div>
-                                        <h3>{data.temp_min} °C</h3></div>
-                                    <div className="box"><h3>Wind Speed</h3>
-                                        <div className="border">
-                                            <img src={wind} className="img" alt="sunrise" />
-                                        </div>
-                                        <h3>{data.windSpeed}</h3></div>
+                                <h3>{data.windSpeed}</h3></div>
 
-                                    <div className="box"><h3>Feels Like</h3>
-                                        <div className="border">
-                                            <img src={cloudy} className="img" alt="weather" />
-                                        </div>
-                                        <h3>{data.desc}</h3></div>
-                                    <div className="box rise"><h3>Sunrise</h3>
-                                        <div className="border">
-                                            <img src={sunrise} className="img" alt="sunrise" />
-                                        </div>
-                                        <h3>{moment(data.sunrise * 1000).format("hh:mm a")}</h3></div>
-                                    <div className="box"><h3>Sunset</h3>
-                                        <div className="border">
-                                            <img src={sunset} className="img" alt="sunset" />
-                                        </div>
-                                        <h3>{moment(data.sunset * 1000).format("hh:mm a")}</h3></div>
-                                    <div className="box"><h3>Pressure</h3>
-                                        <div className="border">
-                                            <img src={pressure} className="img" alt="pressure" />
-                                        </div>
-                                        <h3>{data.pressure}</h3></div>
-                                    <div className="box"><h3>Humidity</h3>
-                                        <div className="border">
-                                            <img src={humidity} className="img" alt="humidity" />
-                                        </div>
-                                        <h3>{data.humidity}</h3></div>
+                            <div className="box"><h3>Feels Like</h3>
+                                <div className="border">
+                                    <img src={`https://openweathermap.org/img/wn/${data.icon}@2x.png`} className="icon-img" alt="" />
+                                    {/* <img src={cloudy} className="img" alt="weather" /> */}
                                 </div>
-                            </>
-                        )}
+                                <h3>{data.desc}</h3></div>
+                            <div className="box rise"><h3>Sunrise</h3>
+                                <div className="border">
+                                    <img src={sunrise} className="img" alt="sunrise" />
+                                </div>
+                                <h3>{moment(data.sunrise * 1000).format("hh:mm a")}</h3></div>
+                            <div className="box"><h3>Sunset</h3>
+                                <div className="border">
+                                    <img src={sunset} className="img" alt="sunset" />
+                                </div>
+                                <h3>{moment(data.sunset * 1000).format("hh:mm a")}</h3></div>
+                            <div className="box"><h3>Pressure</h3>
+                                <div className="border">
+                                    <img src={pressure} className="img" alt="pressure" />
+                                </div>
+                                <h3>{data.pressure}</h3></div>
+                            <div className="box"><h3>Humidity</h3>
+                                <div className="border">
+                                    <img src={humidity} className="img" alt="humidity" />
+                                </div>
+                                <h3>{data.humidity}</h3></div>
+                        </div>
+                    </>
+                    {/* )} */}
                 </div>
             </div>
         </div>
